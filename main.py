7cfdf488d50b433805effe0bd38a6eec9d620d8f
Sriseethaladevi <144207078+Sriseethaladevi@users.noreply.github.com>
2023-09-14 13:02:17 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-# Factorial of a number using recursion
-
-def recur_factorial(n):
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
-
-num = 7
-
-# check if the number is negative
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
-else:
-   print("The factorial of", num, "is", recur_factorial(num))
+class BankAccount:
+    def __init__(self,account_number,account_holder_name,initial_balance=0.0):
+        self.__account_number=account_number
+        self.__account_holder_name=account_holder_name
+        self.__account_balance=initial_balance
+    def deposit (self,amount):
+        if amount>0:
+            self.__account_balance+=amount
+            print ("Deposited rs{}.New balance:rs {}".format (amount,self.__account_balance))
+        else:
+            print ("Invalid deposit amount. Please deposit a positive amount.")
+    def withdraw(self,amount):
+        if amount>0 and amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print ("Withdraw rs {}.New balance: rs {}".format (amount,self.__account_balance))
+        else:
+            print ("Invalid withdrawal amount or insufficient balance.")
+    def display_balance (self):
+        print ("Account balance for {} (Account #{}): rs{}".format (
+            self.__account_holder_name,self.__account_number,
+            self.__account_balance))
+account=BankAccount (account_number="809245679",account_holder_name="Peter",initial_balance=5000.0)
+account.display_balance ()
+account.deposit (500.0)
+account.withdraw (200.0)
+account.display_balance ()
